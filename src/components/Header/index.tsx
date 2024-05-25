e19185d3105ab2794008e5c741eb4bb642d1572e
bilesimomenegali <jbmenegali@gmail.com>
2024-05-25 15:10:43 -0300

    Add Header component and styles for the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,28 @@
+import { ShoppingCart, MapPin } from 'phosphor-react'
+import { Link } from 'react-router-dom'
+import logoCoffeeDelivery from '../../assets/logo-coffee-delivery.svg'
+import {
+  HeaderContainer,
+  RightInfo,
+  ShoppingCartContainer,
+  RegionContainer,
+} from './styles'
+
 export function Header() {
-  return <h1>Header!</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoCoffeeDelivery} height={40} alt="" />
+      <RightInfo>
+        <RegionContainer>
+          <MapPin size={22} weight="fill" />
+          <p>Porto Alegre, RS</p>
+        </RegionContainer>
+        <Link to="/checkout">
+          <ShoppingCartContainer>
+            <ShoppingCart size={22} weight="fill" />
+          </ShoppingCartContainer>
+        </Link>
+      </RightInfo>
+    </HeaderContainer>
+  )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 10rem;
+`
+
+export const RightInfo = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+export const ShoppingCartContainer = styled.div`
+  background: ${(props) => props.theme['yellow-light']};
+  width: 38px;
+  height: 38px;
+  border-radius: 6px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const RegionContainer = styled.div`
+  background: ${(props) => props.theme['purple-light']};
+  padding: 0.5rem 1rem;
+  border-radius: 6px;
+
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['purple-dark']};
+`
